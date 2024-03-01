7d4c7d3ea47ec5cbe5df2027bf69fc7775437bf9
khomboljuice <abz619ygm@gmail.com>
2024-03-01 20:17:07 +0000

    [FIXES] weird spacing in help file

--- help.php
+++ help.php
@@ -7,8 +7,8 @@ Navigate to either of the following scripts:\n
  - removeTable.php          <table_name>                                      to remove a table from the database
  - backupTable.php          <table_name> <backup_name>                        to backup a table
  - mergeBackup.php          <table_name> <backup_name>                        to replace a table with a backup
- - clearRecords.php         <table_name>                                      to clear a table's records\n
- - inputSingleRow.php       <table_name> '<[value, value2, ...]>'             to create a custom row in an existing table
+ - clearRecords.php         <table_name>                                      to clear a table's records
+ - inputSingleRow.php       <table_name> '<[value, value2, ...]>'             to create a custom row in an existing table\n
 ";
 
 exit();
